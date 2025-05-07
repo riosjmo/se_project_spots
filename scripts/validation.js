603de4eb09c52e8a1b548f687ba411b50698c0af
rio <riojordan127@yahoo.com>
2025-05-06 19:22:31 -0500

    add hideinput error and css

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,20 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
-  const errorMsgID = inputEl.id + "-error";
-  const errorMsgEl = document.querySelector("#" + errorMsgID);
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = errorMsg;
-  console.log(errorMsgID);
-}
+  inputEl.classList.add("modal__input_type_error");
+};
+
+const hideInputError = (formEl, inputEl) => {
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = "";
+  inputEl.classList.remove("modal__input_type_error");
+};
 
 const checkInputValidity = (formEl, inputEl) => {
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
   }
 };
 
@@ -21,16 +28,16 @@ const setEventListeners = (formEl) => {
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
       checkInputValidity(formEl, inputElement);
-     // toggleButtonState(inputList, buttonElement);
+      // toggleButtonState(inputList, buttonElement);
     });
   });
-}
+};
 
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll(".modal__form"));
   formList.forEach((formEl) => {
     setEventListeners(formEl);
   });
- };
+};
 
- enableValidation();
+enableValidation();
